29d494f367ea139f6c6a4c5663d93a195481c9c5
Tommy <tommaso.sct@gmail.com>
2016-12-07 07:23:37 +0000

    Added some test and about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  # The rule below maps requests for the URL /static_pages/home to the home action in the StaticPages controller.
+  # Moreover, by using get we arrange for the route to respond to a GET request
   get 'static_pages/home'
-
   get 'static_pages/help'
+  get  'static_pages/about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  #root 'application#hello'
+  
+  # test to root directly home page instead of hello page
+	root 'static_pages#home'
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
+#Add the two lines below for test colors
+require "minitest/reporters"
+Minitest::Reporters.use!
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  # The test below translates to "Let’s test the Home page by issuing a GET request to the 
+  # Static Pages home URL and then making sure we receive a ‘success’ status code in response."
   test "should get home" do
     get static_pages_home_url
     assert_response :success
@@ -8,6 +13,12 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
   test "should get help" do
     get static_pages_help_url
+    assert_response :success
+  
+  #This test is added before adding the about page with the mentality "Test first/ Write failing tests first"
+  end
+    test "should get about" do
+    get static_pages_about_url
     assert_response :success
   end
 

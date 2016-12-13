1dedb0a3a7d511f164a21b5e932d948b086dc137
Tommy <tommaso.sct@gmail.com>
2016-12-13 06:53:30 +0000

    Added layout and user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,20 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   # test to root directly home page instead of hello page
 	root 'static_pages#home'
   # The rule below maps requests for the URL /static_pages/home to the home action in the StaticPages controller.
   # Moreover, by using get we arrange for the route to respond to a GET request
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get  'static_pages/about'
-
+  
+  # get 'static_pages/home'
+  # get 'static_pages/help'
+  # get 'static_pages/about'
+  # get 'static_pages/contact'
+  
+  get  '/help', to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
+  get  '/signup',  to: 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #root 'application#hello'
   
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -13,7 +13,8 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   # The test below translates to "Let’s test the Home page by issuing a GET request to the 
   # Static Pages home URL and then making sure we receive a ‘success’ status code in response."
   test "should get home" do
-    get static_pages_home_url
+    # get static_pages_home_url <-- before changing routes
+    get root_path
     assert_response :success
     # assert_select lets us test for the presence of a particular HTML tag 
     # assert_select "title", "Home | #{@base_title}"
@@ -25,16 +26,23 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_path
     assert_response :success
     assert_select "title", "Help | #{@base_title}"
   
   #This test is added before adding the about page with the mentality "Test first/ Write failing tests first"
   end
-    test "should get about" do
-    get static_pages_about_url
+    
+  test "should get about" do
+    get about_path
     assert_response :success
      assert_select "title", "About | #{@base_title}"
+  end
+  
+  test "should get contact" do
+    get contact_path
+    assert_response :success
+    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
   end
 
 end
